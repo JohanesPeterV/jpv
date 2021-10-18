02df1be5e94333d5551f74effc96c902dd3a768d
JopHme <60497959+JopHme@users.noreply.github.com>
2021-10-18 16:16:24 +0700

    added some stacks

--- src/components/stacks.js
+++ src/components/stacks.js
@@ -5,13 +5,27 @@ const stacks = [
   {
     category: "Programming",
     contents: [
-      { name: "C", link: "https://en.wikipedia.org/wiki/C_(programming_language)" },
+      {
+        name: "C",
+        link: "https://en.wikipedia.org/wiki/C_(programming_language)",
+      },
       { name: "C++", link: "https://en.wikipedia.org/wiki/C%2B%2B" },
-      { name: "C#", link: "https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/" },
+      {
+        name: "C#",
+        link: "https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/",
+      },
+      { name: "R", link: "https://www.r-project.org/" },
       { name: "Java", link: "https://docs.oracle.com/en/java/" },
-      { name: "JavaScript", link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+      {
+        name: "JavaScript",
+        link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+      },
       { name: "TypeScript", link: "https://www.typescriptlang.org/" },
       { name: "Dart", link: "https://dart.dev/guides" },
+      {
+        name: "Kotlin",
+        link: "https://developer.android.com/kotlin?gclid=Cj0KCQjwtrSLBhCLARIsACh6RmhUfKr2tiXSAp33Ka688qBDNF1CIEs53jGAXBkiMzvxFOsQg8of-FQaAv8eEALw_wcB&gclsrc=aw.ds",
+      },
       { name: "Python", link: "https://docs.python.org/3/" },
       { name: "PHP", link: "https://www.php.net/docs.php" },
       { name: "Go", link: "https://golang.org/doc/" },
@@ -20,9 +34,15 @@ const stacks = [
   {
     category: "Database",
     contents: [
-      { name: "SQL Server", link: "https://docs.microsoft.com/en-us/sql/sql-server/?view=sql-server-ver15" },
+      {
+        name: "SQL Server",
+        link: "https://docs.microsoft.com/en-us/sql/sql-server/?view=sql-server-ver15",
+      },
       { name: "MongoDB", link: "https://docs.mongodb.com/" },
-      { name: "Cloud Firestore", link: "https://firebase.google.com/docs/firestore" },
+      {
+        name: "Cloud Firestore",
+        link: "https://firebase.google.com/docs/firestore",
+      },
       { name: "MySQL", link: "https://dev.mysql.com/doc/" },
       { name: "SQLite", link: "https://www.sqlite.org/index.html" },
       { name: "PostgreSQL", link: "https://www.postgresql.org/" },
@@ -35,21 +55,47 @@ const stacks = [
       { name: "HTML, CSS, JS", link: "" },
       { name: "Angular", link: "https://angular.io/docs" },
       { name: "React", link: "https://reactjs.org/docs/getting-started.html" },
-      { name: "Laravel", link: "https://laravel.com/docs/8.x/installation#meet-laravel" },
+      {
+        name: "Laravel",
+        link: "https://laravel.com/docs/8.x/installation#meet-laravel",
+      },
       { name: "Semantic UI React", link: "https://react.semantic-ui.com/" },
       { name: "Bootstrap", link: "https://getbootstrap.com/" },
       { name: "Tailwind CSS", link: "https://tailwindcss.com/" },
+      { name: "Gatsby", link: "https://www.gatsbyjs.com/" },
     ],
   },
   {
     category: "Others",
     contents: [
       { name: "Unity", link: "https://unity.com/" },
-      { name: "Cisco Packet Tracer", link: "https://www.netacad.com/courses/packet-tracer" },
+      {
+        name: "Cisco Packet Tracer",
+        link: "https://www.netacad.com/courses/packet-tracer",
+      },
       { name: "Ubuntu Server", link: "https://ubuntu.com/server/docs" },
+      { name: "Node.js", link: "https://nodejs.org/en/" },
       { name: "Xampp", link: "https://www.apachefriends.org/index.html" },
-      { name: "Adobe Photoshop", link: "https://www.adobe.com/sea/products/photoshop.html?sdid=12B9DXDJ&mv=search&ef_id=Cj0KCQjwtrSLBhCLARIsACh6RmjJ6Virn_0JHpLxMbT7u7jNRR_Ib-u4bCDOq_iaH7LL7ymElzdYFLUaAgg9EALw_wcB:G:s&s_kwcid=AL!3085!3!472466937532!e!!g!!photosho!11350284435!111298133196&gclid=Cj0KCQjwtrSLBhCLARIsACh6RmjJ6Virn_0JHpLxMbT7u7jNRR_Ib-u4bCDOq_iaH7LL7ymElzdYFLUaAgg9EALw_wcB" },
-      { name: "Adobe Premier Pro", link: "https://www.adobe.com/sea/products/premiere.html?sdid=19SCDNXG&mv=search&ef_id=Cj0KCQjwtrSLBhCLARIsACh6RmjRstt3KcY5vpIT96TyizFoqn7aRc3mxi1exQ822Sdqqi_D-6OtjM4aAuryEALw_wcB:G:s&s_kwcid=AL!3085!3!424783761062!e!!g!!premiere%20pro!703952808!38400810458&gclid=Cj0KCQjwtrSLBhCLARIsACh6RmjRstt3KcY5vpIT96TyizFoqn7aRc3mxi1exQ822Sdqqi_D-6OtjM4aAuryEALw_wcB" },
+      {
+        name: "Android",
+        link: "https://developer.android.com/",
+      },
+      {
+        name: "Flutter",
+        link: "https://flutter.dev/?gclid=Cj0KCQjwtrSLBhCLARIsACh6RmjtHujNS1AIhGk-GQXSfuf8gq-KUuiP0CIjFJEBhg0Q5ZjyIybAew8aAmXMEALw_wcB&gclsrc=aw.ds",
+      },
+      {
+        name: "Three JS",
+        link: "https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene",
+      },
+      {
+        name: "Adobe Photoshop",
+        link: "https://www.adobe.com/sea/products/photoshop.html?sdid=12B9DXDJ&mv=search&ef_id=Cj0KCQjwtrSLBhCLARIsACh6RmjJ6Virn_0JHpLxMbT7u7jNRR_Ib-u4bCDOq_iaH7LL7ymElzdYFLUaAgg9EALw_wcB:G:s&s_kwcid=AL!3085!3!472466937532!e!!g!!photosho!11350284435!111298133196&gclid=Cj0KCQjwtrSLBhCLARIsACh6RmjJ6Virn_0JHpLxMbT7u7jNRR_Ib-u4bCDOq_iaH7LL7ymElzdYFLUaAgg9EALw_wcB",
+      },
+      {
+        name: "Adobe Premier Pro",
+        link: "https://www.adobe.com/sea/products/premiere.html?sdid=19SCDNXG&mv=search&ef_id=Cj0KCQjwtrSLBhCLARIsACh6RmjRstt3KcY5vpIT96TyizFoqn7aRc3mxi1exQ822Sdqqi_D-6OtjM4aAuryEALw_wcB:G:s&s_kwcid=AL!3085!3!424783761062!e!!g!!premiere%20pro!703952808!38400810458&gclid=Cj0KCQjwtrSLBhCLARIsACh6RmjRstt3KcY5vpIT96TyizFoqn7aRc3mxi1exQ822Sdqqi_D-6OtjM4aAuryEALw_wcB",
+      },
     ],
   },
 ];
