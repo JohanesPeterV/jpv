36616fe39b89fb34431bdf84410cfe925cec0db7
Johanes Peter Vincentius <JP20-2@slc.net>
2023-05-22 15:30:48 +0700

    feat: add skillstack

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,19 +1,18 @@
-import React from "react"
-import Intro from "./intro"
-import WorkExperience from "./work-experience"
-import Projects from "./projects"
-
+import React from "react";
+import Intro from "./intro";
+import WorkExperience from "./work-experience";
+import Projects from "./projects";
+import StackComponent from "./stacks";
 
 const HomePageContent = () => {
   return (
     <main className="transition-colors duration-300 ">
-      <Intro/>
-      <WorkExperience/>
-      <Projects size={6} title='Github Projects' />
-
-      
+      <Intro />
+      <WorkExperience />
+      <Projects size={6} title="Github Projects" />
+      <StackComponent />
     </main>
-  )
-}
+  );
+};
 
-export default HomePageContent
+export default HomePageContent;
